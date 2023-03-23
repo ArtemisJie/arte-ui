b938155fa56658958b9330930b40d190241a8c0c
ArtemisJie <2875854612@qq.com>
2023-03-23 11:01:50 +0800

    optimize build tools

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,9 +1,9 @@
 /**
-* feature：新功能
+* feat：新功能
 * update：更新某功能
-* fixbug：修补某功能的bug
+* fix：修补某功能的bug
 * refactor：重构某个功能
-* optimize: 优化构建工具或运行时性能
+* perf: 优化构建工具或运行时性能
 * style：仅样式改动
 * docs：仅文档新增/改动
 * chore：构建过程或辅助工具的变动
@@ -14,7 +14,7 @@ module.exports = {
     ],
     rules: {
       'type-enum': [2, 'always', [
-        'feat', 'update', 'fix', 'refactor', 'optimize', 'style', 'docs', 'chore'
+        'feat', 'update', 'fix', 'refactor', 'perf', 'style', 'docs', 'chore'
       ]],
       'type-case': [0],
       'type-empty': [0],
