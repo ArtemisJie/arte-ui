8a1678f9669da18327df18136d010a645afe3aea
ArtemisJie <2875854612@qq.com>
2023-11-11 18:12:57 +0800

    docs: change menu doc

--- src/components/Menu/menu.stories.tsx
+++ src/components/Menu/menu.stories.tsx
@@ -11,26 +11,28 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 //@ts-ignore
-export const ADefaultMenu = (args) => (
-    <Menu defaultIndex='0' {...args} >
-        <Menu.Item index="0">
-            cool link
-        </Menu.Item>
-        <Menu.Item index="1">
-            cool link 2
-        </Menu.Item>
-        <Menu.Item disabled index="2">
-            disabled
-        </Menu.Item>
-        <Menu.SubMenu title="下拉选项" >
-            <Menu.Item index="31">
-                下拉选项一
+export const ADefaultMenu: Story = (args) => (
+    <div style={{ paddingBottom: '2vw' }}>
+        <Menu defaultIndex='0' {...args}>
+            <Menu.Item index="0">
+                cool link
             </Menu.Item>
-            <Menu.Item index="32">
-                下拉选项二
+            <Menu.Item index="1">
+                cool link 2
             </Menu.Item>
-        </Menu.SubMenu>
-    </Menu>
+            <Menu.Item disabled index="2">
+                disabled
+            </Menu.Item>
+            <Menu.SubMenu title="下拉选项">
+                <Menu.Item index="31">
+                    下拉选项一
+                </Menu.Item>
+                <Menu.Item index="32">
+                    下拉选项二
+                </Menu.Item>
+            </Menu.SubMenu>
+        </Menu>
+    </div>
 )
 ADefaultMenu.storyName = '默认Menu'
 //@ts-ignore
